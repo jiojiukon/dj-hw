fe7612a82f34a136a7a4961a3e10d91ed1713954
Evgeniy Osipov <z624153@mail.ru>
2023-06-08 02:00:48 +0400

    finish task

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
 
-from .models import Article
+from django.core.exceptions import ValidationError
+from .models import Article, Tag, Scope
+from django.forms import BaseInlineFormSet
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        counter = 0
+        for form in self.forms:
+            for x, y in form.cleaned_data.items():
+                if x == 'is_main' and y is True:
+                    counter += 1 
+            if counter > 1:
+                raise ValidationError('Основной тэг может быть только один!')
+            if counter == 0:
+                raise ValidationError('Обязательно должен быть основной тэг!')
+        return super().clean() 
+    
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
     pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,16 +1,44 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Тэг')
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+
+    def __str__(self):
+        return self.name
+    
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField(Tag, through='Scope')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        
 
     def __str__(self):
         return self.title
+
+
+    
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='Статьи')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Тэг', related_name='scopes')
+    is_main = models.BooleanField(verbose_name='основной')
+
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статей'
+        ordering = ('-is_main','-tag')
